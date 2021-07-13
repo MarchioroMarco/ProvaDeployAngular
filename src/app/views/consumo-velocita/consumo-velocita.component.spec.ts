5cb09165275fd80ce2d7835edeec5b6855125e30
MarchioroMarco <marco.marchioro@tecnicosuperiorekennedy.it>
2021-07-13 18:50:17 +0200

    update to angular 11

--- src/app/views/consumo-velocita/consumo-velocita.component.spec.ts
+++ src/app/views/consumo-velocita/consumo-velocita.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ConsumoVelocitaComponent } from './consumo-velocita.component';
 
@@ -6,7 +6,7 @@ describe('ConsumoVelocitaComponent', () => {
   let component: ConsumoVelocitaComponent;
   let fixture: ComponentFixture<ConsumoVelocitaComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ConsumoVelocitaComponent ]
     })
--- src/app/views/nastri/nastri.component.spec.ts
+++ src/app/views/nastri/nastri.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NastriComponent } from './nastri.component';
 
@@ -6,7 +6,7 @@ describe('NastriComponent', () => {
   let component: NastriComponent;
   let fixture: ComponentFixture<NastriComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NastriComponent ]
     })
